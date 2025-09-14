ef25728da41be23d0d1d19ba2dad45c9dd7f1211
shivanandu <shivanandu.dev@gmail.com>
2025-09-14 12:23:12 +0530

    third commit

--- app/main.go
+++ app/main.go
@@ -7,33 +7,35 @@ import (
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379:", err)
+		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	defer l.Close() // Ensure the listener is closed when the program exits
-
-	for {
-		conn, err := l.Accept()
-		if err != nil {
-			fmt.Println("Error accepting connection:", err)
-			continue
-		}
-		go handleConnection(conn) // Handle each connection in a goroutine
+	defer l.Close()
+for{
+	conn,err:=l.Accept()
+	if err!=nil{
+		fmt.Println("Error accepting connection: ",err.Error())
+		continue
+	}
+	go handleConnection(conn)
+ }
+}
+func handleConnection(conn net.Conn){
+	defer conn.Close()
+	 _,err := conn.Write([]byte("+PONG\r\n"))
+	if err != nil{
+fmt.Print("Error writing to connection",err)
+return
 	}
 }
+	
+	
 
-func handleConnection(conn net.Conn) {
-	defer conn.Close() // Close the connection when the handler exits
 
-	// Send the RESP-encoded PONG response
-	// The Redis Protocol (RESP) for a simple string "PONG" is "+PONG\r\n"
-	_, err := conn.Write([]byte("+PONG\r\n"))
-	if err != nil {
-		fmt.Println("Error writing to connection:", err)
-	}
-}
+	
+
+
